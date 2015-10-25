9218099d13220d90285eea2e5129d048ff01948c
TheMainBlaine <Blaine@Emei.cartopac.com>
2015-10-25 17:25:06 -0600

    Built MenuDAO.addItemToDB, is yet untested

--- PizzaStore/src/cs414c/pizza/dao/MenuDAO.java
+++ PizzaStore/src/cs414c/pizza/dao/MenuDAO.java
@@ -4,13 +4,14 @@ import cs414c.pizza.domain.*;
 import java.sql.*;
 import java.util.*;
 
-public class MenuDAO{
-	
+public class MenuDAO {
+
 	private Connection connection = null;
-	
-//	private final String GET_PASSWORD_QUERY = "select password from login where username = ?";
-//	private final String INSERT_LOGIN_QUERY = "insert into Login([ID],[USERNAME],[PASSWORD]) VALUES(?,?,?)";
-	
+
+	// private final String GET_PASSWORD_QUERY = "select password from login
+	// where username = ?";
+	private final String INSERT_ITEM_QUERY = "insert into item([ID],[NAME],[BASEPRICE],[DESCRIPTION]) VALUES(?,?,?,?)";
+
 	public MenuDAO() {
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -21,16 +22,15 @@ public class MenuDAO{
 			e.printStackTrace();
 		}
 	}
-	
-	public void generateItemTable(){
+
+	public void generateItemTable() {
 		Statement stmt = null;
 		try {
 			stmt = connection.createStatement();
 			String sql = "CREATE TABLE ITEM " + "([ID] GUID PRIMARY KEY NOT NULL,"
-					+ " [NAME]           	nvarchar(64)    NOT NULL, " 
+					+ " [NAME]           	nvarchar(64)    NOT NULL, "
 					+ " [BASEPRICE]         decimal(5,2) 	NOT NULL, "
-					+ " [DESCRIPTION]		nvarchar(256) 	NOT NULL,"	
-					+ "UNIQUE ([NAME]))";
+					+ " [DESCRIPTION]		nvarchar(256) 	NOT NULL," + "UNIQUE ([NAME]))";
 			stmt.executeUpdate(sql);
 			stmt.close();
 		} catch (Exception e) {
@@ -38,32 +38,64 @@ public class MenuDAO{
 			System.exit(0);
 		}
 	}
-	
-	public void dropAndRecreateItemTable(){
+
+	public void dropAndRecreateItemTable() {
 		Statement stmt = null;
-		try{
+		try {
 			stmt = connection.createStatement();
 			String sql = "Drop Table Item;";
 			stmt.executeUpdate(sql);
 			stmt.close();
-		}catch (Exception e){
+		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
 		generateItemTable();
 	}
-	
-	public void writeMenuToDB(Menu m){
-		Iterator<Item> it = m.ItemSet.iterator();
-	     while(it.hasNext()){
-	        //TODO write insert each item into Item Table
-	     }
+
+//	public void writeMenuToDB(Menu m) {
+//		Iterator<Item> it = m.ItemSet.iterator();
+//		Item i = null;
+//		while (it.hasNext()) {
+//
+//			try {
+//				i = it.next();
+//				PreparedStatement stmt = connection.prepareStatement(INSERT_ITEM_QUERY);
+//				stmt.setString(1, UUID.randomUUID().toString());
+//				stmt.setString(2, i.getName());
+//				stmt.setString(3, "" + i.getBasePrice());
+//				stmt.setString(4, i.getDescription());
+//				if (stmt.executeUpdate() != 1)
+//					return false;
+//			} catch (Exception e) {
+//				System.out.println(e.getMessage());
+//				return false;
+//			}
+//			return true;
+//
+//		}
+//	}
+
+	public boolean addItemToDB(Item i) {
+		//TODO untested
+		try {
+			PreparedStatement stmt = connection.prepareStatement(INSERT_ITEM_QUERY);
+			stmt.setString(1, UUID.randomUUID().toString());
+			stmt.setString(2, i.getName());
+			stmt.setString(3, "" + i.getBasePrice());
+			stmt.setString(4, i.getDescription());
+			if (stmt.executeUpdate() != 1)
+				return false;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
 	}
-	
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		MenuDAO temp = new MenuDAO();
 		temp.dropAndRecreateItemTable();
 	}
-	
+
 }
