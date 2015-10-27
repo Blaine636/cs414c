9bc03576798a0e55c7207bf8a31a637d0c861e9c
aegeland <aegeland@rams.colostate.edu>
2015-10-27 11:42:37 -0600

    One of the three errors fixed
    ya

--- PizzaStore/src/cs414c/pizza/controller/MenuController.java
+++ PizzaStore/src/cs414c/pizza/controller/MenuController.java
@@ -2,13 +2,15 @@ package cs414c.pizza.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cs414c.pizza.dao.MenuDAO;
 import cs414c.pizza.ui.ItemEntry;
 import cs414c.pizza.ui.PizzaEntry;
 import cs414c.pizza.ui.SizeEntry;
 
 public class MenuController {
 	
-	private MenuDAO() mdao = null;
+	private MenuDAO mdao = null;
 
 	public MenuController() {
 		
