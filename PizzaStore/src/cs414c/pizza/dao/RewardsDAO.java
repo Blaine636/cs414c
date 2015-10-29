ff43c9eae8b88b36ec7aa1ead3f0829ee0c035eb
jlarison <jlarison@live.com>
2015-10-28 22:41:55 -0600

    RewardsDAO now closes stuff

--- PizzaStore/src/cs414c/pizza/dao/RewardsDAO.java
+++ PizzaStore/src/cs414c/pizza/dao/RewardsDAO.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 
 public class RewardsDAO {
 
-	private Connection connection = null;
-
 	private final String GET_POINTS_QUERY = "Select Points From Rewards Where RewardID = ?";
 	private final String INSERT_REWARD_QUERY = "Insert Into Rewards(REWARDID,POINTS) VALUES(?,?)";
 	private final String ADD_POINTS_QUERY = "UPDATE Rewards SET Points = Points + ? WHERE RewardID = ?";
@@ -15,19 +13,13 @@ public class RewardsDAO {
 	private final int rewardsScaleFactor = 1000;
 
 	public RewardsDAO() {
-		try {
-			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
-			System.out.println("RewardsDAO opened database successfully");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	public void generateRewardsTable() {
 		Statement stmt = null;
+		Connection connection = null;
 		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
 			stmt = connection.createStatement();
 			String sql = "CREATE TABLE REWARDS " + "([REWARDID] INTEGER PRIMARY KEY NOT NULL,"
 					+ " [POINTS]	INTEGER		NOT NULL)";
@@ -35,27 +27,50 @@ public class RewardsDAO {
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
+		} finally {
+			try {
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
 	public void dropAndRecreateRewardsTable() {
 		Statement stmt = null;
+		Connection connection = null;
 		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
 			stmt = connection.createStatement();
 			String sql = "Drop Table Rewards";
 			stmt.executeUpdate(sql);
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
+		} finally {
+			try {
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 		generateRewardsTable();
 	}
 
 	public double getRewardPoints(int RewardID) {
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
 		try {
-			PreparedStatement stmt = connection.prepareStatement(GET_POINTS_QUERY);
+			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
+			stmt = connection.prepareStatement(GET_POINTS_QUERY);
 			stmt.setInt(1, RewardID);
-			ResultSet rs = stmt.executeQuery();
+			rs = stmt.executeQuery();
 			rs.next();
 			double discountAvailable = ((double)(rs.getInt(1)))/rewardsScaleFactor;
 			double round = discountAvailable*100;
@@ -65,13 +80,28 @@ public class RewardsDAO {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			return -1;
+		} finally {
+			try {
+				stmt.close();
+				rs.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 	}
 
 	public int registerRewardID() {
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
 		try {
-			PreparedStatement stmt = connection.prepareStatement(GET_HIGHEST_ID_QUERY);
-			ResultSet rs = stmt.executeQuery();
+			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
+			stmt = connection.prepareStatement(GET_HIGHEST_ID_QUERY);
+			rs = stmt.executeQuery();
 			int highestID = 0;
 			if (rs.next())
 				highestID = rs.getInt(1);
@@ -82,21 +112,44 @@ public class RewardsDAO {
 				return highestID + 1;
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+				rs.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}
 		return -1;
 	}
 
 	// Adds reward points to RewardID based off of the dollar amount spent
 	public boolean addRewardPoints(int RewardID, double TotalAmountSpent) {
+		
+		 Connection connection = null;
+		 PreparedStatement stmt = null;
+		 
 		try {
-			PreparedStatement stmt = connection.prepareStatement(ADD_POINTS_QUERY);
+			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
+			stmt = connection.prepareStatement(ADD_POINTS_QUERY);
 			stmt.setInt(1, (int) (TotalAmountSpent * 100));
 			stmt.setInt(2, RewardID);
-			if (stmt.executeUpdate() != 1)
-				return false;
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			return false;
+		} finally {
+			try {
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}
 		return true;
 	}
@@ -104,13 +157,19 @@ public class RewardsDAO {
 	// Subtracts reward points until TotalOrderAmount is met or Points reach 0
 	// returns monetary discount achieved by applying rewards
 	public double useRewardPoints(int RewardID, double totalOrderAmount) {
+		
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		
+		
 		double discountAvailable = getRewardPoints(RewardID);
 		double discountToUse = discountAvailable - (discountAvailable - totalOrderAmount);
 		if (discountToUse > discountAvailable) {
 			discountToUse = discountAvailable;
 		}
 		try {
-			PreparedStatement stmt = connection.prepareStatement(USE_POINTS_QUERY);
+			connection = DriverManager.getConnection("jdbc:sqlite:Pizza.db");
+			stmt = connection.prepareStatement(USE_POINTS_QUERY);
 			stmt.setInt(1, (int)(discountToUse*rewardsScaleFactor));
 			stmt.setInt(2, RewardID);
 			if (stmt.executeUpdate() != 1) {
@@ -119,6 +178,14 @@ public class RewardsDAO {
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			return -1;
+		} finally {
+			try {
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		//round to 2 decimal places
