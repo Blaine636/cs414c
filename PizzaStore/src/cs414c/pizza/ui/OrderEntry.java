2ecf446cdad85d5e88f9ff1afa467db9ca932a4c
jlarison <jlarison@live.com>
2015-11-16 13:01:01 -0700

    Added OrderEntry class to decouple chef
    Currently the chef window that blaine worked on is the only one coupled
    to the domain..  working on fixing it

---
+++ PizzaStore/src/cs414c/pizza/ui/OrderEntry.java
@@ -0,0 +1,65 @@
+package cs414c.pizza.ui;
+
+import java.util.List;
+
+public class OrderEntry {
+	
+	private String orderName;
+	private int orderId;
+	private List<ItemEntry> items;
+	
+	public OrderEntry(String orderName, int orderId, List<ItemEntry> items) {
+		this.orderName = orderName;
+		this.orderId = orderId;
+		this.items = items;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public List<ItemEntry> getItems() {
+		return items;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + orderId;
+		result = prime * result + ((orderName == null) ? 0 : orderName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderEntry other = (OrderEntry) obj;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		if (orderId != other.orderId)
+			return false;
+		if (orderName == null) {
+			if (other.orderName != null)
+				return false;
+		} else if (!orderName.equals(other.orderName))
+			return false;
+		return true;
+	}
+	
+	
+
+}
